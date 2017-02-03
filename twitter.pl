ce17d5e1321634de348a385874d1786caa855f4d
Ilan Kleiman <ilankleiman@gmail.com>
2017-02-03 10:33:54 -0500

    Support for merry go round with API Application Keys/Secrets
    Only can list 6,000 followers with Application every 15 minutes. Since
    we’re doing merry go round we can use multiple (unlimited?) api
    keys&secrets to avoid this limit.

--- twitter.pl
+++ twitter.pl
@@ -3,14 +3,20 @@
 use JSON;
 use File::Slurp;
 use MIME::Base64;
+use vars qw(); # global variables... so we can access array of names/keys etc... really is me being lazy
 
-my $followersToList = "ilankleiman";
-my $applicationName = "IlanKleimanApp";
-my $key = "";
-my $secret = "";
+my $followersToList = "artosis";
 
-# pretty static, but 99% will be using multiple keys since twitter api is limiting
-## 99% cus there is a chance that cursor is passable between applications :(
+# merry go round
+our @applicationNames = ("AppName1", "AppName2", "AppName3");
+my $applicationName = @applicationNames[0];
+
+our @keys = ("Key1ForApp1", "Key1ForApp2", "Key1ForApp3");
+my $key = @keys[0];
+
+our @secrets = ("Secret1ForApp1", "Secret1ForApp2", "Secret1ForApp3");
+my $secret = @secrets[0];
+
 my $token = newToken($key, $secret, $applicationName); 
 
 # gets the first chunk (max 200) followers
@@ -27,6 +33,7 @@ elsif(@userListing[1] > 0) {
 	print "Type 'n' to stop\n";
 	print "(y/n): ";
 	chomp(my $answer = <STDIN>);
+	print "This'll take awhile...\n";
 	if($answer =~ /^y|Y$/) {
 		recursiveCaller($token, $applicationName, $followersToList, @userListing[1]);
 	}
@@ -42,6 +49,10 @@ else {
 
 sub newToken {
 	my @parms = @_;
+	if(defined @parms[3]) {
+		@parms[0] = @keys[@parms[3]];
+		@parms[1] = @secrets[@parms[3]];
+	}
 	my $hashed = encode_base64(@parms[0] . ":" . @parms[1]);
 	$hashed =~ s/\n//g;
 	my $res = `curl -s -A "@parms[2]" --header "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" --header "Authorization: Basic $hashed" --data "oauth_consumer_key=@parms[0]" -d "grant_type=client_credentials" "https://api.twitter.com/oauth2/token" -L`;
@@ -68,24 +79,41 @@ sub freshFetch {
 	return @listing;
 }
 
+our $temporaryStorage;
 sub recursiveCaller {
 	my @parms = @_;
+	#token, appName, followersOf, cursor
 	my @res = freshFetch(@parms[0], @parms[1], @parms[2], @parms[3]);
 	if(@res[1] > 0) {
 		append_file("followers.txt", @res[0]);
+		$temporaryStorage = @res[1]; # temporarily store most recent valid cursor; so we can pass it to a working key if current key maxes out
 		recursiveCaller(@parms[0], @parms[1], @parms[2], @res[1]);
 	}
 	else {
-		# looks like we need to do this one last time:
 		append_file("followers.txt", @res[0]);
 
 		my $exit = @res[1];
 		if($exit =~ /^$/) {
-			$exit = "max requests";
+			$exit = "Max requests reached!\n\n";
 		}
 		print "Looks like we've reached the end!\n" . $exit;
-		#sleep(2);
-		#append_file("followers.txt", "END: " . $exit);
+		## position of current app name (0)+1
+		$j = 0;
+		$found = -1;
+		for($j; $j < scalar(@applicationNames); $j++) {
+			if(@applicationNames[$j] =~ /^@parms[1]$/) {
+				$found = $j
+			}
+		}
+
+		if(scalar(@applicationNames) > $found+1) {
+			print "Looks like we can proceed with another API key\n";
+			recursiveCaller(newToken(0, 0, @applicationNames[$found+1], $found+1), @applicationNames[$found+1], @parms[2], $temporaryStorage);
+		}
+		else {
+			print "Used all ". ($found+1) ." API keys available.\n";
+			exit;
+		}
 	}
 }
 
